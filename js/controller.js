26cf15941c0ba3f4406a95f222aecd6ec0cb8316
Hashed Hyphen <hashedhyphen@gmail.com>
2016-03-06 13:57:17 +0900

    Dentoo.LT #13 initial commit

---
+++ js/controller.js
@@ -0,0 +1,123 @@
+document.addEventListener(`DOMContentLoaded`, ev => {
+  const $$ = query => document.querySelectorAll(query),
+        container = $$(`.container`)[0],
+        slides    = $$(`.slide`);
+
+  const slideMap = new Map();
+  for (let i = 0; i < slides.length; i += 1) {
+    slideMap.set(slides[i].id, i);
+  }
+
+  let currentIndex = 0;
+
+  const goToSlideAt = nextIndex => {
+    if (nextIndex < 0 || slides.length <= nextIndex) { return false; }
+
+    slides[currentIndex].style.opacity = 0;
+    slides[currentIndex].style.zIndex  = 0;
+
+    slides[nextIndex].style.opacity = 1;
+    slides[nextIndex].style.zIndex  = 1;
+
+    currentIndex = nextIndex;
+    return true;
+  };
+
+  const updateProgressBar = () => {
+    const bar = $$(`.progress`)[0],
+          ratio = `${currentIndex / (slides.length - 1)}`;
+    bar.style.width = `calc(${container.clientWidth}px * ${ratio}`;
+    return true;
+  };
+
+  const goToSlideOf = slideID => {
+    const index = slideMap.get(slideID);
+    if (index === undefined) { return false; } // unknown slideID
+
+    return goToSlideAt(index) && updateProgressBar();
+  };
+
+  window.addEventListener(`hashchange`, ev => {
+    const slideID = location.hash.slice(1);
+    goToSlideOf(slideID);
+  }, false);
+
+  // keyboard events and listeners
+  const goForward = () => {
+    if (currentIndex >= slides.length - 1) { return; }
+
+    const slideID = slides[currentIndex + 1].id;
+    location.hash = slideID;
+  };
+
+  const goBack = () => {
+    if (currentIndex <= 0) { return; }
+
+    const slideID = slides[currentIndex - 1].id;
+    location.hash = slideID;
+  };
+
+  document.addEventListener(`keydown`, ev => {
+    if (ev.key) {
+      switch (ev.key) {
+        case `ArrowRight`:
+        case `ArrowDown`:
+        case `l`:
+        case `j`:
+        case `Enter`:
+        case ` `:
+          ev.preventDefault();
+          return goForward();
+
+        case `ArrowLeft`:
+        case `ArrowUp`:
+        case `h`:
+        case `k`:
+        case `Backspace`:
+          ev.preventDefault();
+          return goBack();
+
+        // no default
+      }
+    } else if (ev.keyCode) {
+      switch (ev.keyCode) {
+        case 39:
+        case 40:
+        case 76:
+        case 74:
+        case 13:
+        case 32:
+          ev.preventDefault();
+          return goForward();
+
+        case 37:
+        case 38:
+        case 72:
+        case 75:
+        case  8:
+          ev.preventDefault();
+          return goBack();
+
+        // no default
+      }
+    }
+  }, false);
+
+  // resize events and listeners
+  const adjustScale = () => {
+    const denominator = Math.max(
+      container.clientWidth  / window.innerWidth,
+      container.clientHeight / window.innerHeight
+    );
+    container.style.transform = `scale(${1 / denominator})`;
+  };
+
+  window.addEventListener(`resize`, adjustScale, false);
+
+  // first view
+  hljs.initHighlightingOnLoad();
+  adjustScale();
+  if (!goToSlideOf(location.hash.slice(1))) {
+    location.hash = slides[0].id;  // default
+  }
+});
